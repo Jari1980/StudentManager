d86ebb360fb6173c5d685e4a7c80b4d4b3959f2c
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-01-09 10:29:44 +0100

    part 1 working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.config.ComponentScanConfig;
+import org.example.data_access.StudentDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
+
     public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        StudentDao studentDao = context.getBean(StudentDao.class);
+
 
         System.out.println("Broccoli!!");
     }
